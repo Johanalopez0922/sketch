0bb54fdad2dfc36eb3260932611f793f55fce6a5
lucianamaya <128107361+lucianamaya@users.noreply.github.com>
2023-05-09 21:05:22 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function setup() {
+    createCanvas(400,400);
+    engine = Engine.create();
+    world = Engine.world;
+    
+
+    var ball_options={
+        restitution:0.95,
+        frictionAir:0.01
+
+    }
+
+    var rectangle_options={
+        isStatic:true
+    }
+
+
+    ball=Bodies.circle(100,10,20,ball_options);
+    World.add(world,ball);
+    rectangle1=Bodies.rectangle(200,350,150,50,rectangle_options);
+    World.add(world,rectangle1);
+}
+
+function draw(){
+  background(51);
+  Engine.update(engine);
+  ellipse(ball.position.x,ball.position.y,200);
+  rect(rectangle1.position.x,rectangle1.position.y,500,20);
+}
+
